9ee96b80347f757188a8c733ee86d195e36f72cd
Rajat Sharma <rjtsharma3538@gmail.com>
2023-09-03 11:22:55 +0530

    Update 3.cpp

--- Sep 2023/3.cpp
+++ Sep 2023/3.cpp
@@ -1,1 +1,31 @@
+// this is gfg problem of the day of 3 Sept 2023
+// problem link : https://practice.geeksforgeeks.org/problems/check-if-tree-is-isomorphic/1
 
+class Solution{
+  public:
+  
+    bool isIsomorphic(Node *root1,Node *root2)
+    {
+        if(!root1&&!root2)
+            return true;
+        if(!root1)
+            return false;
+        if(!root2)
+            return false;
+        if(root1->data!=root2->data)
+            return false;
+            
+        bool l1,r1,l2,r2;
+        l1=isIsomorphic(root1->left,root2->right);
+        r1=isIsomorphic(root1->right,root2->left);
+        
+        l2=isIsomorphic(root1->left,root2->left);
+        r2=isIsomorphic(root1->right,root2->right);
+        
+        if(l1&r1)
+            return true;
+        if(l2&r2)
+            return true;
+        return false;
+    }
+};
