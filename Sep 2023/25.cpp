3c7ce30d69b75c6cea0669f35f6670e93243b21f
Rajat Sharma <rjtsharma3538@gmail.com>
2023-09-25 22:57:15 +0530

    Update 25.cpp

--- Sep 2023/25.cpp
+++ Sep 2023/25.cpp
@@ -1,24 +1,32 @@
-// this is leetcode daily challenge of 25 Sept 2023
-// problem link : https://leetcode.com/problems/find-the-difference/description/?envType=daily-question&envId=2023-09-25
+// this is gfg problem of the day of 25 Sept 2023
+// problem link : https://practice.geeksforgeeks.org/problems/maximum-sum-combination/1
 
 class Solution {
-public:
-    char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-
-        int i=0,j=0;
-
-        while(i<s.size()&&j<t.size())
+  public:
+    vector<int> maxCombinations(int N, int K, vector<int> &A, vector<int> &B) {
+        sort(A.begin(),A.end());
+        sort(B.begin(),B.end());
+        priority_queue<pair<int,int>> pq;
+        
+        for(int i=0;i<N;i++)
         {
-            if(s[i]==t[j])
-            {
-                i++;
-                j++;
-            }
-            else 
-                return t[j];
+            pq.push({A[i]+B[N-1],N-1});
         }
-        return t[j];
+        
+        vector<int> ans;
+        
+        while(K--&&!pq.empty())
+        {
+            int sum=pq.top().first;
+            int idx=pq.top().second;
+            pq.pop();
+            
+            ans.push_back(sum);
+            
+            if(idx-1>=0)
+                pq.push({sum-B[idx]+B[idx-1],idx-1});
+        }
+        
+        return ans;
     }
 };
