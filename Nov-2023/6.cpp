06484ea4dde7e646d703c0f58597fbfc07854757
Rajat Sharma <rjtsharma3538@gmail.com>
2023-11-06 10:44:50 +0530

    Update 6.cpp

--- Nov-2023/6.cpp
+++ Nov-2023/6.cpp
@@ -1,29 +1,49 @@
-// this is leetcode daily challenge of 6 Nov 2023
-// problem link : https://leetcode.com/problems/seat-reservation-manager/description/?envType=daily-question&envId=2023-11-06
+// this is gfg problem of the day of 6 Nov 2023
+// problem link : https://www.geeksforgeeks.org/problems/c-letters-collection4552/1
 
-class SeatManager {
+class Solution{
 public:
-
-    set<int> res,un;
-    SeatManager(int n) {
-        for(int i=1;i<=n;i++)
-            res.insert(i);
-    }
-    
-    int reserve() {
-        int num=*(res.begin());
-        res.erase(num);
-        return num;
-    }
-    
-    void unreserve(int seatNumber) {
-        res.insert(seatNumber);
+    vector<int> matrixSum(int n, int m, vector<vector<int>> mat, int q, vector<int> queries[])
+    {
+        int dr1[]={-1,-1,-1,0,0,1,1,1};
+        int dc1[]={-1,0,1,-1,1,-1,0,1};
+        
+        int dr2[]={2,2,2,2,2,-2,-2,-2,-2,-2,0,0,-1,-1,1,1};
+        int dc2[]={-2,-1,0,1,2,-2,-1,0,1,2,-2,2,-2,2,-2,2};
+        
+        vector<int> v;
+        
+        for(int i=0;i<q;i++)
+        {
+            int ans=0;
+            int r=queries[i][1],c=queries[i][2];
+            if(queries[i][0]==1)
+            {
+                for(int j=0;j<8;j++)
+                {
+                    int new_r=r+dr1[j];
+                    int new_c=c+dc1[j];
+                    
+                    if(new_r<n&&new_c<m&&new_r>=0&&new_c>=0)
+                        ans+=mat[new_r][new_c];
+                }
+            }
+            
+            else 
+            {
+                for(int j=0;j<16;j++)
+                {
+                    int new_r=r+dr2[j];
+                    int new_c=c+dc2[j];
+                    
+                    if(new_r<n&&new_c<m&&new_r>=0&&new_c>=0)
+                        ans+=mat[new_r][new_c];
+                }
+            }
+            
+            v.push_back(ans);
+        }
+        
+        return v;
     }
 };
-
-/**
- * Your SeatManager object will be instantiated and called as such:
- * SeatManager* obj = new SeatManager(n);
- * int param_1 = obj->reserve();
- * obj->unreserve(seatNumber);
- */
